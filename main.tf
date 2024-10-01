86d4e00c72d48fd1928d617eda798517d1d8dbfb
Kevin Ledesma <kevin.ledesma@infopro-digital.com>
2024-10-01 22:17:51 +0200

    aft_feature_delete_default_vpcs_enabled = true

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -15,4 +15,6 @@ module "aft" {
   account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+
+  aft_feature_delete_default_vpcs_enabled = true
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "875775009908"
+log_archive_account_id      = "710271919625"
+audit_account_id            = "266735823831"
+aft_management_account_id   = "888577057148"
+github_username             = "kev-01"
+ct_home_region              = "eu-west-3"
+tf_backend_secondary_region = "eu-west-1"
